e85db328b138f760a28e6fbdba4ce636926f7e59
Paul Kim <paulkim517@gmail.com>
2015-07-21 12:18:34 -0700

    login/signup page on

--- public/js/route_magic.js
+++ public/js/route_magic.js
@@ -24,5 +24,23 @@ jQuery(document).ready(function($) {
 		return false;
 	});	
 
-	
+	// `mainController` holds shared site functionality
+  var mainController = {
+    // compile underscore template for nav links
+    navTemplate: _.template($('#nav-template').html()),
+    // get current (logged-in) user
+    console.log(navTemplate)
+    showCurrentUser: function() {
+      // AJAX call to server to GET /api/users/current
+      $.get('/api/users/current', function(user) {
+        console.log(user);
+        // pass current user through template for nav links
+        $navHtml = $(mainController.navTemplate({currentUser: user}));
+        // append nav links HTML to page
+        $('#nav-links').append($navHtml);
+      });
+    }
+  };
+
+  mainController.showCurrentUser();
 });
