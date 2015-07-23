a3168462e22b601dba11756d83763d32194cd3fa
Paul Kim <paulkim517@gmail.com>
2015-07-22 20:32:36 -0700

    external html pages link

--- public/js/route_magic.js
+++ public/js/route_magic.js
@@ -9,13 +9,14 @@ var section_show_time = 1300;
 jQuery(document).ready(function($) {
 
 	// Switch section
-	$("a", '.mainmenu').click(function() 
+	$('.yolo').click(function() 
 	{
 		if( ! $(this).hasClass('active') ) { 
+			console.log('im clicking');
 			current_item = this;
 			// close all visible divs with the class of .section
 			$('.section:visible').fadeOut( section_hide_time, function() { 
-				$('a', '.mainmenu').removeClass( 'active' );  
+				$('.yolo').removeClass( 'active' );  
 				$(current_item).addClass( 'active' );
 				var new_section = $( $(current_item).attr('href') );
 				new_section.fadeIn( section_show_time );
